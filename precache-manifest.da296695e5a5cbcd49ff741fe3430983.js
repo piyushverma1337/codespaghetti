84b68e18d9128e0cda928c576b55b9f24ce855fc
Piyush Verma <piyushverma005@gmail.com>
2020-08-06 20:10:20 +0530

    Updates

---
+++ precache-manifest.da296695e5a5cbcd49ff741fe3430983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab816458bdf4a5229ad7b96fac5d3fd3",
+    "url": "/codespaghetti/index.html"
+  },
+  {
+    "revision": "262cd9c2c968862f6abf",
+    "url": "/codespaghetti/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1fab8a267b1099c33e90",
+    "url": "/codespaghetti/static/js/2.a36f52a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/codespaghetti/static/js/2.a36f52a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262cd9c2c968862f6abf",
+    "url": "/codespaghetti/static/js/main.a1370fe4.chunk.js"
+  },
+  {
+    "revision": "f46615746deef1701be1",
+    "url": "/codespaghetti/static/js/runtime-main.d49cbfb7.js"
+  }
+]);
